71dd1313ae4828ec5d32e336dd161c983cb755a1
Philip Munksgaard <philip@munksgaard.me>
2025-07-01 22:47:05 +0200

    Add minimal flake.nix with a devShell

--- lib/mix/tasks/flakify.ex
+++ lib/mix/tasks/flakify.ex
@@ -109,6 +109,31 @@ if Code.ensure_loaded?(Igniter) do
          false
          """)}
       )
+      |> Igniter.create_new_file("flake.nix", """
+      {
+        inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+        outputs = inputs:
+          let
+            supportedSystems =
+              [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+            forEachSupportedSystem = f:
+              inputs.nixpkgs.lib.genAttrs supportedSystems
+              (system: f { pkgs = import inputs.nixpkgs { inherit system; }; });
+
+          in {
+            devShells = forEachSupportedSystem ({ pkgs }: {
+              default = pkgs.mkShell {
+                packages =
+                  (if pkgs.stdenv.isLinux then [ pkgs.inotify-tools ] else [ ]) ++
+                    [ pkgs.elixir pkgs.tailwindcss_4 pkgs.esbuild pkgs.go ];
+                MIX_TAILWIND_PATH = "${pkgs.tailwindcss_4}/bin/tailwindcss";
+                MIX_ESBUILD_PATH = "${pkgs.esbuild}/bin/esbuild";
+              };
+            });
+          };
+      }
+      """)
     end
   end
 else
