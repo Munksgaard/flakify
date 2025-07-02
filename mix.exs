50958d7e2c3b7399d351ce24cdcf9be0bdf7b9a1
Philip Munksgaard <philip@munksgaard.me>
2025-07-02 08:51:00 +0200

    Set up hex package documentation

--- mix.exs
+++ mix.exs
@@ -1,13 +1,30 @@
 defmodule Flakify.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @scm_url "https://github.com/Munksgaard/flakify"
+  @elixir_requirement "~> 1.18"
+
   def project do
     [
       app: :flakify,
-      version: "0.1.0",
-      elixir: "~> 1.18",
+      version: @version,
+      elixir: @elixir_requirement,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: [
+        maintainers: [
+          "Philip Munksgaard"
+        ],
+        licenses: ["MIT"],
+        links: %{"GitHub" => @scm_url},
+        files: ~w(lib mix.exs README.md)
+      ],
+      source_url: @scm_url,
+      docs: docs(),
+      description: """
+      Sets up a Nix flake-based development environment for your Elixir/Phoenix project.
+      """
     ]
   end
 
@@ -21,7 +38,14 @@ defmodule Flakify.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:igniter, "~> 0.6", optional: true}
+      {:igniter, "~> 0.6", optional: true},
+      {:ex_doc, "~> 0.24", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url_pattern: "#{@scm_url}/blob/v#{@version}/%{path}#L%{line}"
     ]
   end
 end
